ca36574cf61622bac8a68448a766fa324fcea5ec
Greg Yaniw <gyaniw@Administrators-MacBook-Pro.local>
2024-07-03 23:39:50 -0400

    Set game timer to 60 seconds

--- src/constants.js
+++ src/constants.js
@@ -2,5 +2,5 @@ export const NORTH = 'NORTH';
 export const SOUTH = 'SOUTH';
 export const EAST = 'EAST';
 export const WEST = 'WEST';
-export const GAME_TIME = 10;
+export const GAME_TIME = 60;
 export const GAME_OVER = 'Game Over!';
